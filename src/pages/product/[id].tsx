bae5461f2f371742352f690ae5f2a0e9b85df5f7
Pedro Martinelli <p.martinellinunes@gmail.com>
2023-02-27 15:58:53 -0300

    finalizada a estrutura e estilização da página do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,26 @@
 import { useRouter } from "next/router"
+import Image from "next/image"
+
+import { ProductContainer, ImageContainer, ProductDetails } from "@/styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>produto: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+      
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus ex minus reiciendis explicabo voluptate voluptatem inventore, consequuntur dolorem reprehenderit praesentium earum accusantium aut fuga tempore cum et? Aut, tempore fugiat!</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
